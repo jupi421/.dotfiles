8d6887dd16a2292f964bf383901b9aff01086a19
jaycon03 <justinpils@icloud.com>
2024-03-02 13:17:30 +0100

    test

--- nvim/lua/jay/lazy/telescope.lua
+++ nvim/lua/jay/lazy/telescope.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require('telescope').setup({})
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 		vim.keymap.set('n', '<leader>fs', function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
