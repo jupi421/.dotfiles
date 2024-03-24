f3e2fa8b3ddf8d400620649ca03a78d7bdd44e17
jaycon03 <justinpils@icloud.com>
2024-03-24 16:34:17 +0100

    ignore log files

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -4,8 +4,8 @@
 	programs.tmux = {
 		enable = true;
 		shell = "${pkgs.zsh}/bin/zsh";
-		terminal = "tmux-256-color";
-		historyLimit = 100000;
+		prefix = "C-Space";
+		mouse = true;
 		plugins = with pkgs; [
 			{
 				plugin = tmuxPlugins.catppuccin;
@@ -15,6 +15,20 @@
 					set -g @catppuccin_date_time "%H:%M"
 				'';
 			}
+			{
+				plugin = tmuxPlugins.sensible;
+			}
 		];
+		extraConfig = '' 
+			# navigation between windows
+			bind -n M-H previous-window
+			bind -n M-L next-window
+
+			# window indexing from 1
+			set -g base-index 1
+			set -g pane-base-index 1
+			set-window-option -g pane-base-index 1
+			set-option -g renumber-windows on
+		'';
 	};
 }
