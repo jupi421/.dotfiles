9df0441b07051305ec0691e4999cc73d0a0efb06
jaycon03 <pilsj00@gmail.com>
2024-06-01 11:40:57 +0200

    added espresso

---
+++ programs/extern/espressoMD/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "espressoMD";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    cython3-0-9.url = "github:nixos/nixpkgs/92d295f588631b0db2da509f381b4fb1e74173c5";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+	let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs {
+			inherit system;
+		};
+		cython = inputs.cython3-0-9.legacyPackages.${system}.python311Packages.cython;
+		boostMpi = pkgs.boost.override {
+			useMpi = true;
+			mpi = pkgs.openmpi;
+		};
+	in {
+		packages.${system}.espressoMD = pkgs.stdenv.mkDerivation {
+			pname = "espressoMD"; 
+			version = "4.2.2";
+			src = pkgs.fetchFromGitHub {
+				owner = "espressomd";
+				repo = "espresso";
+				rev = "4.2.2";
+				sha256 = "sha256-f/rPURva1zx1hoqG3GZ/K3FvKtiLp+qz9Q6BeZBN0lY=";
+			};
+
+			buildInputs = with pkgs; [
+				cmake
+				cython
+				python3
+				openmpi
+				fftw
+				fftwMpi
+				boostMpi
+				hdf5
+				python311Packages.numpy
+				python311Packages.scipy
+				python311Packages.pyopengl
+				gsl
+				freeglut
+			];
+
+			buildPhase = ''
+				cmake ..
+				make -j$(nproc)
+			'';
+
+			installPhase = ''
+				make install PREFIX=$out
+			'';
+		};
+	};
+}
--- test/overlay/cython-overlay.nix
+++
@@ -1,8 +0,0 @@
-self: super: {
-  cython = super.callPackage (super.fetchFromGitHub {
-    owner = "cython";
-    repo = "cython";
-    rev = "3.0.8";
-    sha256 = "0w0w8yk9scip7kjmlabmxy3xfq7yxsd4qx53hwhx83wi4qgzdn8n";
-  }) {};
-}
