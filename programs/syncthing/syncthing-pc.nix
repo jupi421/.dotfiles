df74eb1e8e74ce8551724305dfd911f416870bcb
jupi421 <pilsj00@gmail.com>
2024-06-05 17:21:23 +0200

    updated syncthing key laptop

--- programs/syncthing/syncthing-pc.nix
+++ programs/syncthing/syncthing-pc.nix
@@ -10,7 +10,7 @@
 		overrideFolders = true;
 		settings = {
 			devices = {
-				"laptop" = { id = "OQI6ZK4-HN5NVQF-BHCLEB3-ZZ6GLV6-HH3CL4B-CJF76LJ-GDHUP6Y-KSJQHQ2"; };
+				"laptop" = { id = "NJ2EMHW-PM3CV5E-UGU5KS6-IWQ6YS4-XC5VWUO-JRU6LFL-2R35DLI-OH7MRQL"; };
 			};
 			folders = {
 				"Documents" = {
