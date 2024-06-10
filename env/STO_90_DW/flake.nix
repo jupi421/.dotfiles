fdaaedb1e4e0e631607627f5749f88378f46ae1b
jupi421 <pilsj00@gmail.com>
2024-06-10 14:00:06 +0200

    packaged vasputil

--- env/STO_90_DW/flake.nix
+++ env/STO_90_DW/flake.nix
@@ -1,29 +1,34 @@
 {
-	description = "Basic python env";
+	description = "STO_90_DW";
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+		vasputil-flake.url = "path:/home/jay/.dotfiles/programs/extern/vasputil";
 	};
 
-	outputs = { self, nixpkgs, flake-utils, ... }:
+	outputs = { self, nixpkgs, flake-utils, vasputil-flake }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = import nixpkgs {
 					inherit system;
 				};
 
+				vasputil = vasputil-flake.packages.${system}.vasputil;
+
 				pythonEnv = pkgs.python3.withPackages (ps: with ps; [
 					numpy
 					matplotlib
 					scipy
-					tkinter
 					ase
 				]);
 			in
 				{
 					devShell = pkgs.mkShell {
-						buildInputs = [ pythonEnv ];
+						buildInputs = [
+							pythonEnv 
+							vasputil 
+						];
 					};
 				}
 		);
---
+++ programs/extern/vasputil/flake.nix
@@ -0,0 +1,38 @@
+{
+	description = "vasputil";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+	outputs = { self, nixpkgs }: 
+		let
+			system = "x86_64-linux";
+			pkgs = import nixpkgs {
+				inherit system;
+			};
+			pythonPackages = pkgs.python3Packages;
+			vasputil-src = pkgs.fetchFromGitHub {
+				owner = "jabl";
+				repo = "vasputil";
+				rev = "6.1";
+				sha256 = "sha256-W7nDhzW9dUxgjHd1+1b3wEacg9AQnozBN1R423Xd6c8=";
+			};
+		in {
+			packages.${system}.vasputil = pythonPackages.buildPythonPackage {
+				pname = "vasputil";
+				version = "6.1";
+				src = vasputil-src;
+
+				propagatedBuildInputs = with pythonPackages; [
+					numpy
+					ase
+					matplotlib
+					scipy
+				];
+
+				postPatch = ''
+					substituteInPlace setup.py --replace "version='master'" "version='6.1'"
+				'';
+			};
+	};
+}
