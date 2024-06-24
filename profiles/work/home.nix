fec7a50282365514b2530c195689bde09d6ccbec
jaycon03 <pilsj00@gmail.com>
2024-06-24 21:57:58 +0200

    zsh nix module

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -1,148 +1,151 @@
 {config, pkgs, inputs, ... }:
 
 {
-  imports = [
-	inputs.xremap-flake.homeManagerModules.default
-	../../programs/tmux/tmux.nix
-	../../programs/nvim/default.nix
-  ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "jay";
-  home.homeDirectory = "/home/jay";
+	imports = [
+		inputs.xremap-flake.homeManagerModules.default
+			../../programs/tmux/tmux.nix
+			../../programs/nvim/default.nix
+			../../programs/zsh/default.nix
+	];
+# Home Manager needs a bit of information about you and the paths it should
+# manage.
+	home.username = "jay";
+	home.homeDirectory = "/home/jay";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+# This value determines the Home Manager release that your configuration is
+# compatible with. This helps avoid breakage when a new Home Manager release
+# introduces backwards incompatible changes.
+#
+# You should not change this value, even if you update Home Manager. If you do
+# want to update the value, then make sure to first check the Home Manager
+# release notes.
+	home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-   
-  services.xremap = {
-	  withX11 = true;
-	  #yamlConfig = ''
-	  #  modmap:
-	  #    - name: Global
-	  #      remap:
-	  #  	  CapsLock:
-	  #  	    held: CTRL_L
-	  #  		alone: Esc
-	  #  		alone_timeout_millis: 500
-	  #'';
-	  config = {
-	      modmap = [
-	    	{
-	    		name = "Capslock to Ctrl";
-	    		remap = {
-	    			"CapsLock" = "CTRL_L";
-	    		};
-	    	}
-	      ];
-	  };
-  };
-  
-  programs.direnv = {
-	  enable = true;
-	  enableZshIntegration = true;
-	  nix-direnv.enable = true;
-  };
+# The home.packages option allows you to install Nix packages into your
+# environment.
 
-  programs.zsh = {
-    enable = true;
-  };
+		services.xremap = {
+			withX11 = true;
+#yamlConfig = ''
+#  modmap:
+#    - name: Global
+#      remap:
+#  	  CapsLock:
+#  	    held: CTRL_L
+#  		alone: Esc
+#  		alone_timeout_millis: 500
+#'';
+			config = {
+				modmap = [
+				{
+					name = "Capslock to Ctrl";
+					remap = {
+						"CapsLock" = "CTRL_L";
+					};
+				}
+				];
+			};
+		};
 
-  programs.zsh.enableCompletion = true;
-  home.sessionVariables.SHELL = pkgs.zsh;
+	programs.direnv = {
+		enable = true;
+		enableZshIntegration = true;
+		nix-direnv.enable = true;
+	};
 
-  programs.starship = {
-   enable = true;
-   enableZshIntegration = true;
-  };
+#  programs.zsh = {
+#    enable = true;
+#  };
+#
+#  programs.zsh.enableCompletion = true;
 
-  home.packages = with pkgs; [
-    blueman
-    brightnessctl
-	dmenu
-	dunst
-	fd
-#	inputs.tmux-sessionx.packages.x86_64-linux.default
+	home.sessionVariables = {
+		SHELL = pkgs.zsh;
+		EDITOR = "nvim";
+	};
+#
+#  programs.starship = {
+#   enable = true;
+#   enableZshIntegration = true;
+#  };
 
-	firefox
-	flameshot
-	fzf
-	feh
-	gdb
-	gnuplot
-	htop
-	kitty
-	obsidian
-	picom
-	qalculate-gtk
-	python311Packages.qtile
-	ripgrep
-	rofi
-	sassc
-	starship
-	spotify
-	teams-for-linux
-	telegram-desktop
-	tree
-	nitrogen
-	notify
-	whatsapp-for-linux
-	webcord
-	wmctrl
-	xclip
-	xournalpp
-	zoxide
-	zsh
-	zathura
-	zip
-	zoom-us
-	
-	# fonts
-	iosevka
-	nerdfonts
-	terminus-nerdfont
-	font-awesome
-  ];
+	home.packages = with pkgs; [
+		blueman
+		brightnessctl
+		dmenu
+		dunst
+		fd
+		firefox
+		flameshot
+		fzf
+		feh
+		gdb
+		gnuplot
+		htop
+		kitty
+		obsidian
+		picom
+		qalculate-gtk
+		python311Packages.qtile
+		ripgrep
+		rofi
+		sassc
+		starship
+		spotify
+		teams-for-linux
+		telegram-desktop
+		tree
+		nitrogen
+		notify
+		whatsapp-for-linux
+		webcord
+		wmctrl
+		xclip
+		xournalpp
+		zoxide
+		zsh
+		zathura
+		zip
+		zoom-us
 
-  fonts.fontconfig.enable = true;
+# fonts
+		iosevka
+		nerdfonts
+		terminus-nerdfont
+		font-awesome
+	];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-  	#".config/nvim".source = config.lib.file.mkOutOfStoreSymlink /home/jay/.dotfiles/nvim/nvim;
-	".config/qtile".source = ../../programs/qtile;
-	".config/picom".source = ../../programs/picom;
-	".config/kitty".source = ../../programs/kitty;
-	".config/dunst".source = ../../programs/dunst;
-	".config/rofi".source = ../../programs/rofi;
-	".config/zathura".source = ../../programs/zathura;
-	".zshrc".source = ../../programs/.zshrc;
-  };
+	fonts.fontconfig.enable = true;
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jay/etc/profile.d/hm-session-vars.sh
-  #
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
+	home.file = {
+#".config/nvim".source = config.lib.file.mkOutOfStoreSymlink /home/jay/.dotfiles/nvim/nvim;
+		".config/qtile".source = ../../programs/qtile;
+		".config/picom".source = ../../programs/picom;
+		".config/kitty".source = ../../programs/kitty;
+		".config/dunst".source = ../../programs/dunst;
+		".config/rofi".source = ../../programs/rofi;
+		".config/zathura".source = ../../programs/zathura;
+#".zshrc".source = ../../programs/.zshrc;
+	};
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+# Home Manager can also manage your environment variables through
+# 'home.sessionVariables'. If you don't want to manage your shell through Home
+# Manager then you have to manually source 'hm-session-vars.sh' located at
+# either
+#
+#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  /etc/profiles/per-user/jay/etc/profile.d/hm-session-vars.sh
+#
+
+# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
---
+++ programs/zsh/default.nix
@@ -0,0 +1,103 @@
+{ config, nixpkgs, ... }:
+
+{
+	programs.zsh = {
+		enable = true;
+		enableCompletion = true;
+		initExtra = /*bash*/''
+			# Lines configured by zsh-newuser-install
+			HISTFILE=~/.histfile
+			HISTSIZE=10000
+			SAVEHIST=10000
+
+			autoload -U compinit
+			zstyle ':completion:*' menu select
+			zmodload zsh/complist
+			compinit
+			_comp_options+=(globdots) #include dotfiles
+
+			# vim mode
+			bindkey -v
+			export KEYTIMEOUT=20
+
+			# vim keys in drop down menu
+			bindkey -M menuselect 'h' vi-backward-char
+			bindkey -M menuselect 'k' vi-up-line-or-history
+			bindkey -M menuselect 'l' vi-forward-char
+			bindkey -M menuselect 'j' vi-down-line-or-history
+			bindkey jj vi-cmd-mode
+
+			# aliases
+			alias v="nvim"
+			alias l="ls --color=auto -l"
+			alias la="ls --color=auto -la"
+			#alias jukit_kitty="kitty --listen-on=unix:@"$(date +%s%N)" -o allow_remote_control=yes"
+			alias tn="tmux-new"
+			alias ts="tmux new -s"
+
+			export FZF_DEFAULT_OPTS=" \
+			--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+			--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+			--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+			#load zsh syntax highlighting
+			source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
+
+			if [ -n "$${commands[fzf-share]}" ]; then
+			  source "$(fzf-share)/completion.zsh"
+			fi
+
+			find-directories-widget() {
+				local dir
+				dir=$(fd --type d | fzf ) || return
+				LBUFFER+="$dir"
+			}
+
+			find-hidden-directories-widget() {
+				local dir
+				dir=$(fd --type d --hidden | fzf) || return
+				LBUFFER+="$dir"
+			}
+
+			find-command-hist-widget() {
+				local command
+				command=$(history / | fzf | awk '{$1=""; print $0}') || return
+				LBUFFER+="$command"
+			}
+
+			find-file-widget() {
+				local file
+				file=$(fd --type f | fzf) || return
+				LBUFFER+="$file"
+			}
+
+			zle -N find-directories-widget
+			zle -N find-hidden-directories-widget
+			zle -N find-command-hist-widget
+			zle -N find-file-widget
+
+			bindkey '^t' find-directories-widget
+			bindkey '^[c' find-hidden-directories-widget
+			bindkey '^r' find-command-hist-widget
+			bindkey '^f' find-file-widget
+
+			source /usr/share/autojump/autojump.zsh 2>/dev/null
+
+			tmux-new() {
+				session=$(sesh list | fzf)
+				sesh connect "$session"
+			}
+
+			stty -ixon
+
+			eval "$(starship init zsh)"
+			eval "$(zoxide init zsh)"
+			eval "$(direnv hook zsh)"
+		'';
+	};
+
+	programs.starship = {
+		enable = true;
+		enableZshIntegration = true;
+	};
+}
