454e03dcd618097f4dca08f3b3eb748da056a7cd
jaycon03 <pilsj00@gmail.com>
2024-09-26 20:48:00 +0200

    pybind11-stubgen

--- env/STO_90_DW/flake.nix
+++ env/STO_90_DW/flake.nix
@@ -4,10 +4,11 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+		pybind11-stubgen-flake.url = "path:/home/jay/.dotfiles/programs/extern/pybind11-stubgen/";
 #		vasputil-flake.url = "path:/home/jay/.dotfiles/programs/extern/vasputil";
 	};
 
-	outputs = { self, nixpkgs, flake-utils}: #, vasputil-flake }:
+	outputs = { self, nixpkgs, flake-utils, pybind11-stubgen-flake, ... }@inputs: #, vasputil-flake }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = import nixpkgs {
@@ -15,7 +16,7 @@
 				};
 
 #				vasputil = vasputil-flake.packages.${system}.vasputil;
-
+				pybind11-stubgen = pybind11-stubgen-flake.packages.${system}.pybind11-stubgen;
 				pythonEnv = pkgs.python3.withPackages (ps: with ps; [
 					numpy
 					matplotlib
@@ -23,13 +24,25 @@
 					ase
 					pybind11
 				]);
+
 			in
 				{
 					devShell = pkgs.mkShell {
+						nativeBuildInputs = with pkgs; [
+							pkg-config 
+							cmake
+						];
+
 						buildInputs = [
 							pythonEnv 
+							pybind11-stubgen
 #							vasputil 
 						];
+
+						# add compile_flags.txt for clangd linting
+						shellHook = ''
+							echo "$(python -m pybind11 --includes | awk '{ for (i = 1; i <= NF; i++ ) print $i }')" > compile_flags.txt
+						'';
 					};
 				}
 		);
