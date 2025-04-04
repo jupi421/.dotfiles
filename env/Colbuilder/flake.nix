4e1a20e4bb92544701acc6b8cd577b85a352265f
jupi421 <pilsj00@gmail.com>
2025-04-04 09:53:42 +0200

    working colbuilder env

--- env/Colbuilder/flake.nix
+++ env/Colbuilder/flake.nix
@@ -1,27 +1,29 @@
 {
-	description = "Collagen Project";
+  description = "NixOS flake with a conda dev environment for numpy and matplotlib";
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
 
-	outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+	outputs = { self, nixpkgs, flake-utils, ... }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
-				pkgs = import nixpkgs {
-					inherit system;
-					config.allowUnfree = true;
+				pkgs = import nixpkgs 
+					{ 
+						inherit system;
+						config.allowUnfree = true;
 				};
-			in
-				{
-					devShells.default = pkgs.mkShell {
 
-						buildInputs = [ 
-							pkgs.conda
-							pkgs.steam-run
-						];
-					};
-				}
-		);
+			in {
+				devShells.default = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						conda
+						steam-run
+						gromacsPlumed
+						freetype
+						xorg.libXScrnSaver
+					];
+				};
+			});
 }
