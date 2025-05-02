c9148b18ca68bf90452ec930bf55640d025db1f2
jupi421 <pilsj00@gmail.com>
2025-05-02 10:03:27 +0200

    lsp

--- profiles/office/home.nix
+++ profiles/office/home.nix
@@ -11,6 +11,9 @@ in {
 		../../programs/waybar/waybar.nix
 		../../programs/kitty/kitty.nix
 	];
+	
+	
+	home.stateVersion = "23.11"; # Please read the comment before changing.
 
 	home.username = "jay";
 	home.homeDirectory = "/home/jay";
--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -11,22 +11,11 @@ in {
 		../../programs/waybar/waybar.nix
 		../../programs/kitty/kitty.nix
 	];
-	# Home Manager needs a bit of information about you and the paths it should
-	# manage.
+	
 	home.username = "jay";
 	home.homeDirectory = "/home/jay";
 
-	# This value determines the Home Manager release that your configuration is
-	# compatible with. This helps avoid breakage when a new Home Manager release
-	# introduces backwards incompatible changes.
-	#
-	# You should not change this value, even if you update Home Manager. If you do
-	# want to update the value, then make sure to first check the Home Manager
-	# release notes.
 	home.stateVersion = "23.11"; # Please read the comment before changing.
-
-	# The home.packages option allows you to install Nix packages into your
-	# environment.
 
 	home.sessionVariables = {
 		EDITOR = "nvim";
